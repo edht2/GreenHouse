f4c31d9317f111743a01a762edb8d2e2234f7d9a
Ed Haig-Thomas <ehaigthomas@gmail.com>
2024-05-26 19:27:10 +0100

    Remastered the webpage

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask,  render_template
-
 app = Flask('app')
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template("main.html")
+  
+@app.route('/green-house')
+def greenhouse():
+    return render_template("greenhouse.html")
+  
+@app.route('/calendar')
+def calendar():
+    return render_template("calendar.html")
+  
+@app.route('/todo-list')
+def todo():
+    return render_template("todo.html")
+  
+@app.route('/test')
+def test():
+    return render_template("test.html")
+  
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")
+
 
 app.run(host='0.0.0.0', port=8080)
