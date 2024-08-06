a92e6a583722dfca6549d95af67bda5b600fd578
Edht <109654331+edht2@users.noreply.github.com>
2024-08-06 11:23:26 +0100

    Delete SensorPi2 directory

--- SensorPi2/app/extensions/utils.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-class utils:
-    def fire_and_forget(f):
-        def wrapped(*args, **kwargs):
-            return asyncio.get_event_loop().run_in_executor(None, f, *args, *kwargs)
-            # runs the targeted function on a seperate thread
-        return wrapped
--- SensorPi2/app/main/mqtt.py
+++
@@ -1,7 +0,0 @@
-from app.extensions.mqtt import pub
-from app.main.conf import climateZone
-
-class MQTT:
-    def send_sensor_data(self, data):
-        pub.publish(f'SYS/ClimateZone{climateZone()}', message=data)
-        # sends sensor data to the broker
