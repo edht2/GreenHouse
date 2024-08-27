a64a67398c079c62290a197eb3d81deea1e46d16
Ed Haig-Thomas <ehaigthomas@gmail.com>
2024-08-27 15:07:03 +0100

    Tweaked the run file

--- ControllerPi/run.py
+++ ControllerPi/run.py
@@ -1,3 +1,6 @@
+from colorama import Style, Fore, Back
+print(f"{Back.GREEN}{Fore.WHITE}Initiating GreenHouse...{Style.RESET_ALL}")
+
 from app.extensions.mqtt import pub, sub
 from app.extensions.utils import utils
 from app.main.greenhouse import GreenHouse
@@ -5,6 +8,7 @@ from app.state.greenhouse import onConfigRequest, GREENHOUSE
 
 @utils.fire_and_forget
 def setupResponse(data):
+    print("GERBSISDADASD")
     config = onConfigRequest(data)
     pub.publish('SYS/setup', config)
     # the climate zone should change based on the current climatezones
